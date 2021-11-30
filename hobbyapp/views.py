719b036119b7aa101f1c1573933313c2f2e809e3
Yasir <yasirmabdullah1@hotmail.com>
2021-11-30 15:46:26 +0000

    basic login added

--- hobbyapp/views.py
+++ hobbyapp/views.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render
 from django.utils import timezone
+from django.http.response import HttpResponse, HttpResponseForbidden
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from django.utils import timezone
+
+from hobbyapp.forms import UserForm
 # Create your views here.
 def index(request):
     
     return render(request, 'hobbyapp/hobbyapp.html',{
         'title': "Hobbies App",
+    })
+
+def login_view(request):
+    if request.method == "POST":
+        user = authenticate(
+            request,
+            username=request.POST['username'],
+            password=request.POST['password'],
+        )
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            return HttpResponseForbidden("Invalid credentials")
+
+    return render(request, 'hobbyapp/login.html', {
+        'form': UserForm()
     })
