494b4578851c23f0045fd841fdfa0e925f53d50d
Yasir <yasirmabdullah1@hotmail.com>
2021-11-30 17:21:00 +0000

    added test users

---
+++ hobbyapp/createTestUsers.py
@@ -0,0 +1,4 @@
+from .models import User
+
+u = User("yasir","yasir",10)
+u.save()
--- hobbyapp/views.py
+++ hobbyapp/views.py
@@ -23,7 +23,7 @@ def login_view(request):
         )
         if user is not None:
             login(request, user)
-            return redirect('index')
+            return index(request)
         else:
             return HttpResponseForbidden("Invalid credentials")
 
