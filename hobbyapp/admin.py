6c3f5b70dc689c497b962404e439108164595d16
Yasir Abdullah <yasirmabdullah1@hotmail.com>
2021-12-01 19:16:00 +0000

    finished part 2

--- hobbyapp/admin.py
+++ hobbyapp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import User
+from .models import Hobby, User
 
 admin.site.register(User)
+admin.site.register(Hobby)
 # Register your models here.
--- hobbyapp/models.py
+++ hobbyapp/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+import datetime
 # Create your models here
 
+class Hobby(models.Model):
+    name = models.CharField(max_length=200)
+
+
 class User(AbstractUser):
-    age = models.IntegerField
+    profileImage = models.ImageField(upload_to = 'profile_pic',default = 'media/default_pic.png')
+    email = models.EmailField()
+    city = models.CharField(max_length=200)
+    dateOfBirth = models.DateField(default=datetime.date.today)
+    hobbies = models.ManyToManyField(Hobby)
+    
+
