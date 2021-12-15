20a8f4cdfb6005c83621304cc24ccc6bc0112ecb
Abdul989 <abdulbaasitshariff@gmail.com>
2021-12-15 18:53:57 +0000

    trusted the origin

--- hobbysite/settings.py
+++ hobbysite/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = ['https://.deploytest-deploytest.apps.kube.eecs.qmul.ac.uk', "https://.127.0.0.1"]
+CSRF_TRUSTED_ORIGINS = ['https://django-psql-persistent-group46.apps.kube.eecs.qmul.ac.uk/', "https://.127.0.0.1"]
 
 # Application definition
 
